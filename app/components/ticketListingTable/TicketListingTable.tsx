715f4cff0730a0c0ccb6bc5c763e2d4db972ed78
umairrazam <omairrr.azam@gmail.com>
2024-11-13 01:10:16 +0500

    fix: empty tickets handling

--- app/components/ticketListingTable/TicketListingTable.tsx
+++ app/components/ticketListingTable/TicketListingTable.tsx
@@ -3,6 +3,7 @@
 import { useGetTicketsQuery } from "@/lib/features/tickets/ticketsApiSlice";
 import { TicketListingItem } from "../ticketListingItem/TicketListingItem";
 import { LoadingSkeleton } from "./LoadingSkeleton";
+import Link from "next/link";
 
 export const TicketListingTable = () => {
   const { data, isError, isLoading, isSuccess } = useGetTicketsQuery(100);
@@ -10,23 +11,30 @@ export const TicketListingTable = () => {
   return (
     <>
       {isSuccess ? (
-        <table className="w-full lg:w-3/4 text-left bg-white">
-          <thead className="bg-white">
-            <tr>
-              <th className="dm-mono font-medium text-[13.02px] pb-[15.14px]">
-                Ticket
-              </th>
-              <th className="dm-mono font-medium text-[13.02px] italic pl-[19.72px] pb-[15.14px]">
-                Time Logged
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {data?.ticket.map((d, index) => (
-              <TicketListingItem key={`${index}`} ticket={d} />
-            ))}
-          </tbody>
-        </table>
+        <>
+          {data?.ticket?.length === 0 && (
+            <h1>
+              No tickets created yet, lets <Link className="text-base font-medium text-center dm-mono underline" href={`/tickets/new`}>Create</Link>
+            </h1>
+          )}
+          {data?.ticket?.length > 0 && <table className="w-full lg:w-3/4 text-left bg-white">
+            <thead className="bg-white">
+              <tr>
+                <th className="dm-mono font-medium text-[13.02px] pb-[15.14px]">
+                  Ticket
+                </th>
+                <th className="dm-mono font-medium text-[13.02px] italic pl-[19.72px] pb-[15.14px]">
+                  Time Logged
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.ticket.map((d, index) => (
+                <TicketListingItem key={`${index}`} ticket={d} />
+              ))}
+            </tbody>
+          </table>}
+        </>
       ) : isLoading ? (
         <LoadingSkeleton />
       ) : (
