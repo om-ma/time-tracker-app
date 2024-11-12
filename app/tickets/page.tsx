134be99df3d1a8d738e8a48da57b40877ebe8aaa
ibrarzahid975 <ibrarzahid975@gmail.com>
2024-11-12 17:46:29 +0500

    home page (#4)
    Co-authored-by: OMZ <77246192+umairrazam@users.noreply.github.com>

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -3,26 +3,26 @@ import Link from "next/link";
 export default function TicketssPage() {
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <div className="w-full max-w-md p-6 bg-gray-50 rounded-lg shadow-md">
-          <h2 className="text-center text-lg font-semibold text-gray-800 mb-4">
+      <div className="bg-Neutral-100 p-[31px] h-[637px]">
+        <div className="">
+          <h2 className="text-base font-medium text-center dm-mono">
             Select an existing ticket to log time
           </h2>
-          <p className="text-center text-sm text-gray-600 mb-6">
-            Or{" "}
-            <Link className="text-blue-600 underline" href={`/tickets/new`}>
-              Create a new one
+          <p className="text-base font-medium text-center dm-mono mt-[15px]">
+            Or{" "} <br />
+            <Link className="text-base font-medium text-center dm-mono underline" href={`/tickets/new`}>
+              Create a new one       
             </Link>
           </p>
 
-          <div className="space-y-4">
+          <div className="space-y-8 mt-[72px]">
             <div className="w-full h-12 bg-gray-300 rounded-md"></div>
             <div className="w-full h-12 bg-gray-300 rounded-md"></div>
             <div className="flex space-x-4">
               <div className="w-1/2 h-12 bg-gray-300 rounded-md"></div>
               <div className="w-1/2 h-12 bg-gray-300 rounded-md"></div>
             </div>
-          </div>
+          </div>      
         </div>
       </div>
     </>
